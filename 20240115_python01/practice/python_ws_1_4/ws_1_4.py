09b98abff6c28ea519bcac0aaad1c1869994a6d8
JHS <wkgytmd200@naver.com>
2024-01-15 17:54:23 +0900

    20240115 academy final_commit

--- 20240115_python01/practice/python_ws_1_4/ws_1_4.py
+++ 20240115_python01/practice/python_ws_1_4/ws_1_4.py
@@ -4,9 +4,15 @@ import math
 3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679
 # 반지름
 15
+
+
 pi = math.pi
 radius = 15
-print('원주율 : ', pi)
-print('반지름 : ', radius)
-print('원의 둘레 : ', radius * 2 * math.pi)
-print('원의 넓이 : ', radius ** 2 * math.pi)
+pi_is = '원주율 : '
+radius_is = '반지름 : '
+perimeter_is = '원의 둘레 : '
+area_is = '원의 넓이 : '
+print(pi_is, pi)
+print(radius_is, radius)
+print(perimeter_is, radius * 2 * math.pi)
+print(area_is, radius ** 2 * math.pi)
