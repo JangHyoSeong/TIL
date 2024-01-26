4e0b37606f2bab744eda6a102e0ed881a1b5a83d
JHS <wkdgytmd200@naver.com>
2024-01-26 20:06:28 +0900

    관통프로젝트 2주차 완료

--- 99_pjt/02_pjt/02_pjt/movie/spotify/problem_e.py
+++ 99_pjt/02_pjt/02_pjt/movie/spotify/problem_e.py
@@ -7,7 +7,49 @@ def recommendation(track, artist, genre):
     # 여기에 코드를 작성합니다.
     URL = 'https://api.spotify.com/v1'
     headers = getHeaders()
-    pass
+    params = {
+        'q': f'track:{track}',  # 필수 파라미터
+        'type': 'track',  # 필수 파라미터
+        'market': 'KR',
+        'limit': 20,
+    }
+
+    response_track = requests.get(f'{URL}/search', headers=headers, params=params)
+    response_track = response_track.json()
+    track_id = response_track['tracks']['items'][0]['id']
+    
+
+    params = {
+        'q': f'artist:{artist}',  # 필수 파라미터
+        'type': 'artist',  # 필수 파라미터
+        'market': 'KR',
+        'limit': 20,
+    }
+
+    response_artist = requests.get(f'{URL}/search', headers=headers, params=params)
+    response_artist = response_artist.json()
+
+    artist_id = response_artist['artists']['items'][0]['id']
+
+    params = {
+        'seed_artists' : artist_id,
+        'seed_genres' : genre,
+        'seed_trakcs' : track_id,
+        'limit' : 10,
+        'market' : 'KR'
+    }
+
+    response_recommended = requests.get(f'{URL}/recommendations', headers=headers, params=params)
+    response_recommended = response_recommended.json()
+
+    recommended = []
+    song_list = response_recommended['tracks']
+    for song in song_list:
+        recommended.append(song['name'])
+
+    return recommended
+
+    
 
 
 # 아래의 코드는 수정하지 않습니다.
