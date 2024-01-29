3d537cc05fd85a24167ccd27b3670074c48d41a8
JHS <wkdgytmd200@naver.com>
2024-01-29 20:24:31 +0900

    20240129 문제풀이

--- 02_algorithm/20240129_argorithm01/hw/1206_View/sol.py
+++ 02_algorithm/20240129_argorithm01/hw/1206_View/sol.py
@@ -1,11 +1,15 @@
 T = 10
 
-for testcase in range(T+1):
+for testcase in range(1, T+1):
     width = int(input())
     buildings = list(map(int, input().split()))
 
-    height_sum = 0
+    view_point = 0
 
-    for height in range(1, width -2):
-        for i in range(height-2, height+3):
-            pass
+    for height in range(2, width -2):
+        max_height = max(buildings[height - 2], buildings[height - 1], buildings[height + 1], buildings[height + 2])
+        if buildings[height] >= max_height:
+             view_point += buildings[height] - max_height
+    
+    print(f'#{testcase} {view_point}')
+            
