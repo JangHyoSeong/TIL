57c6a822ee2f7fb3a13be3d3a95ccc37190789ed
JHS <wkdgytmd200@naver.com>
2024-01-30 17:49:31 +0900

    20240130 알고리즘 부분합 주석수정

--- 02_algorithm/20240129_argorithm01/hw/4835_구간합/sol.py
+++ 02_algorithm/20240129_argorithm01/hw/4835_구간합/sol.py
@@ -8,14 +8,14 @@ for testcase in range(1, T+1):
     N, M = map(int, (input().split()))
     numbers = list(map(int, input().split()))
 
-    max = 0
-    min = 2 ** 31 - 1
+    max = 0             # 최대값
+    min = 2 ** 31 - 1   # 최소값
 
     for i in range(N - M + 1):
         arr_sum = 0
 
         for j in range(M):
-            arr_sum += numbers[i + j]
+            arr_sum += numbers[i + j]   # 구간합 구함
 
         if max < arr_sum:
             max = arr_sum
