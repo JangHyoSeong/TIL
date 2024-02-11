e37e4f9341d1ba80e4771e22b17b27e8e6e96714
JHS <wkdgytmd200@naver.com>
2024-02-11 20:42:06 +0900

    5432_쇠막대기_자르기 풀이

--- 02_algorithm/20240208_Stack/hw/5432_쇠막대기_자르기/sol.py
+++ 02_algorithm/20240208_Stack/hw/5432_쇠막대기_자르기/sol.py
@@ -1,3 +1,21 @@
 import sys
 sys.stdin = open('input.txt')
 
+T = int(input())
+
+for testcase in range(1, T+1):
+
+    lazer = list(input())
+    str_len = len(lazer)
+    steel = 0
+    count = 0
+
+    for i in range(str_len-1):
+        if lazer[i] == '(' and lazer[i+1] == '(':
+            count += 1
+            steel += 1
+        elif lazer[i] == '(' and lazer[i+1] == ')':
+            steel += count
+        elif lazer[i] == ')' and lazer[i+1] == ')':
+            count -= 1
+    print(f'#{testcase} {steel}')
