dc1a4fe963ee0d52b49abe8683a4aa8a7fd42258
JHS <wkdgytmd200@naver.com>
2024-02-25 18:21:00 +0900

    파리퇴치 주석추가

--- 02_algorithm/99_etc/파리퇴치_remake/solution.py
+++ 02_algorithm/99_etc/파리퇴치_remake/solution.py
@@ -14,10 +14,14 @@ for testcase in range(1, T+1):
         for j in range(N-M+1):
             temp_kill = 0
             
+
+            # 일단 M x M 크기 만큼 모두 더함
             for x in range(M):
                 for y in range(M):
                     temp_kill += flies[i+x][j+y]
+            
 
+            # 구멍이 뚫린 만큼 다시 뺌
             for x in range(1, M-1):
                 for y in range(1, M-1):
                     temp_kill -= flies[i+x][j+y]
