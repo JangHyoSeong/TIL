d6faa34529343d0bc1e4f0951bbf321dd48c26b2
JHS <wkdgytmd200@naver.com>
2024-02-29 09:06:46 +0900

    4408 자기 방으로 돌아가기 풀이

--- 02_algorithm/20240214_Stack2/hw/4408_자기_방으로_돌아가기/sol.py
+++ 02_algorithm/20240214_Stack2/hw/4408_자기_방으로_돌아가기/sol.py
@@ -1,33 +1,28 @@
 import sys
 sys.stdin = open('input.txt')
 
-def calcMinTime(rooms):
-
-    time = 0
-    
-    for room, student in rooms.items():
-        time = max(time, len(student))
-
-    return time
-
-
-
-
 T = int(input())
 
 for testcase in range(1, T+1):
 
     N = int(input())
-
     move = [list(map(int, input().split())) for _ in range(N)]
-    rooms = {room: [] for room in range(1, 401)}
+    
+    for i in range(N):
+        # 숫자가 작은 방에서 큰 방으로 이동하게끔 설정
+        if move[i][0] > move[i][1]:
+            move[i][0], move[i][1] = move[i][1], move[i][0]
+        # 짝수방을 모두 홀수방으로 옮김
+        if move[i][0]%2==0:
+            move[i][0] -= 1
+        if move[i][1]%2==0:
+            move[i][1] -= 1
+
+    # 복도 리스트. 학생이 지나갈때마다 1이 증가. 최대값이 되는 부분의 값이 걸리는 시간
+    corridor = [0] * 402
 
     for i in range(N):
-        start, end = move[i]
-        rooms[start].append(end)
-        rooms[end].append(start)
+        for j in range(move[i][0], move[i][1]+1):
+            corridor[j] += 1
 
-    time = calcMinTime(rooms)
-
-
-    print(f'#{testcase} {time}')
+    print(f'#{testcase} {max(corridor)}')
