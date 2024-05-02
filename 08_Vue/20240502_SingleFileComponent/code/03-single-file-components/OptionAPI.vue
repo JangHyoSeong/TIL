a616b87868dc95ace8f429875c1dd6f8dac3d1d1
JHS <wkdgytmd200@naver.com>
2024-05-02 11:08:13 +0900

    20240502 필기

---
+++ 08_Vue/20240502_SingleFileComponent/code/03-single-file-components/OptionAPI.vue
@@ -0,0 +1,23 @@
+<template>
+  <button @click="increment">{{ count }}</button>
+</template>
+
+<script>
+export default {
+  data () {
+    return{
+      count: 0
+    }
+  },
+
+  methods: {
+    increment(){
+      this.count++
+    }
+  },
+
+  mounted() {
+    console.log(`숫자 세기의 초기값은 ${this.count}`)
+  }
+}
+</script>
---
+++ 08_Vue/20240502_SingleFileComponent/code/vue-project/src/components/MyComponentItem.vue
@@ -0,0 +1,6 @@
+<template>
+  <p>MyComponentItem</p>
+</template>
+
+<script setup>
+</script>
